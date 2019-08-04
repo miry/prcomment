877b830edef24b5578bfa04491cb35e2b7e00af8
Michael Nikitochkin <miry.sof@gmail.com>
2019-08-05 00:03:28 +0200

    Extract to seaprate class logic related to Github API

---
+++ src/github/client.cr
@@ -0,0 +1,72 @@
+require "http/client"
+require "json"
+
+module Github
+  class Client
+    def initialize(@token : String, @repo = "miry/prcomment", @issue = 1)
+      @http = HTTP::Client.new "api.github.com", port: 443, tls: true
+    end
+
+    def comments
+      response = @http.get "/repos/#{@repo}/issues/#{@issue}/comments"
+
+      if response.status_code != 200 # => 200
+        puts "Something goes wrong"
+        puts response.status.code
+        return [] of String
+      end
+      if response.body?
+        return JSON.parse(response.body)
+      end
+      puts "Something goes wrong"
+      puts response.status_code
+      return [] of String
+    end
+
+    def create_comment(msg : String)
+      response = @http.post("/repos/#{@repo}/issues/#{@issue}/comments",
+        headers: HTTP::Headers{"Authorization" => "token #{@token}"},
+        body: %({"body": "#{msg}"))
+
+      case response.status_code
+      when 201
+        puts "201: Successfuly created a comment"
+      when 401
+        puts "401: Requires authentication"
+      when 403
+        puts "403: Does not have access"
+        puts response.body.lines
+      else
+        puts "Something goes wrong"
+        puts response.status.code
+        puts response.body.lines
+      end
+      nil
+    end
+
+    # https://developer.github.com/v3/issues/comments/#edit-a-comment
+    def update_comment(id, msg : String)
+      response = @http.patch("/repos/#{@repo}/issues/comments/#{id}",
+        headers: HTTP::Headers{"Authorization" => "token #{@token}"},
+        body: %({"body": "#{msg}"))
+      case response.status_code
+      when 200
+        puts "200: Successfuly updated the comment"
+      when 401
+        puts "401: Requires authentication"
+      when 403
+        puts "403: Does not have access"
+        puts response.body.lines
+      else
+        puts "Something goes wrong"
+        puts response.status.code
+        puts response.body.lines
+      end
+      nil
+    end
+
+    def close
+      @http.close unless @http
+    end
+  end
+end
--- src/prcomment.cr
+++ src/prcomment.cr
@@ -1,98 +1,47 @@
-require "json"
-require "http/client"
+require "./github/*"
 
 class PRComment
-	@client : HTTP::Client
-	def initialize
-		@client = gh_client
-	end
+  @client : Github::Client
 
-	def my_comment(pattern : Regex)
-		result = comments
-		if result.is_a?(JSON::Any)
-			result.as_a.each do |comment|
-				if pattern.match(comment["body"].to_s)
-					return comment
-				end
-			end
-		end
-		nil
-	end
+  def initialize(token : String)
+    @client = Github::Client.new(token)
+  end
 
-	def comments
-		response = @client.get "/repos/miry/prcomment/issues/1/comments"
+  def my_comment(pattern : Regex)
+    result = @client.comments
+    if result.is_a?(JSON::Any)
+      result.as_a.each do |comment|
+        if pattern.match(comment["body"].to_s)
+          return comment
+        end
+      end
+    end
+    nil
+  end
 
-		if response.status_code != 200     # => 200
-			puts "Something goes wrong"
-			puts response.status.code
-			return [] of String
-		end
-		if response.body?
-			return JSON.parse(response.body)
-		end
-		puts "Something goes wrong"
-		puts response.status_code
-		return [] of String
-	end
+  def post(msg : String)
+    comment = my_comment(/#{msg}/i)
+    if comment
+      update_comment(comment["id"], msg)
+    else
+      create_comment(msg)
+    end
+  end
 
-	# https://developer.github.com/v3/issues/comments/#create-a-comment
-	def create_comment(token, msg : String)
-		response = @client.post("/repos/miry/prcomment/issues/1/comments",
-					headers: HTTP::Headers{"Authorization" => "token #{token}"},
-					body: %({"body": "#{msg}"))
+  def update_comment(comment_id, msg : String)
+    @client.update_comment(comment_id, msg)
+  end
 
-		case response.status_code
-		when 201
-			puts "201: Successfuly created a comment"
-		when 401
-			puts "401: Requires authentication"
-		when 403
-			puts "403: Does not have access"
-			puts response.body.lines
-		else
-			puts "Something goes wrong"
-			puts response.status.code
-			puts response.body.lines
-		end
-		nil
-	end
+  def create_comment(msg : String)
+    @client.create_comment(msg)
+  end
 
-	# https://developer.github.com/v3/issues/comments/#edit-a-comment
-	def update_comment(token, id, msg : String)
-		response = @client.patch("/repos/miry/prcomment/issues/comments/#{id}",
-					headers: HTTP::Headers{"Authorization" => "token #{token}"},
-					body: %({"body": "#{msg}"))
-		case response.status_code
-		when 200
-			puts "200: Successfuly updated the comment"
-		when 401
-			puts "401: Requires authentication"
-		when 403
-			puts "403: Does not have access"
-			puts response.body.lines
-		else
-			puts "Something goes wrong"
-			puts response.status.code
-			puts response.body.lines
-		end
-		nil
-	end
-
-	def gh_client
-		HTTP::Client.new "api.github.com", port: 443, tls: true
-	end
-
-	def close
-		@client.close unless @client.nil?
-	end
+  def close
+    @client.close unless @client.nil?
+  end
 end
 
 token = ENV["GITHUB_TOKEN"]
-tool = PRComment.new
-comment = tool.my_comment(/First/i)
-if comment
-	tool.update_comment(token, comment["id"], "Seccond comment")
-else
-	tool.create_comment(token, "First comment")
-end
+tool = PRComment.new(token)
+tool.post("First comment")
 tool.close
